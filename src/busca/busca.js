a7c729ca805a5d9a126af988606fcad36fb4f43a
leandrojr2012 <106202719+leandrojr2012@users.noreply.github.com>
2023-11-21 22:41:22 -0300

    .

--- src/busca/busca.js
+++ src/busca/busca.js
@@ -9,7 +9,7 @@ async function busca_data(){
 
     let data_final = data_ano + '-' + data_mes + '-' + data_dia
     
-    let data_teste = 2023-12-17
+    let data_teste = '2023-12-17'
 
     const data = await db.select('entrada_not_cob_data_recebimento')
                          .from('entrada_notificacao_cobranca')
@@ -17,17 +17,21 @@ async function busca_data(){
         for(let i in data){
             if(data){
                 let nova_data = data[i].entrada_not_cob_data_recebimento
-            console.log(nova_data.slice(0,9))
+                let nova_dia = nova_data.getDate().toString()
+                let nova_mes = (nova_data.getMonth()+1).toString()
+                let nova_ano = nova_data.getFullYear().toString()
+                let data_banco = nova_ano+'-'+nova_mes+'-'+nova_dia
+
+                if(data_teste == data_banco){
+                    await db('entrada_notificacao_cobranca')
+                    .where({entrada_status_desc_identrada : entrada_status_desc_identrada})
+                    .update({entrada_not_cob_email_enviado:1})
+                }
+                
             }
         }
-
-
-    if(data == data_teste){
-        console.log('teste ok')
-    }
 
 }
 
 busca_data()
-
 
