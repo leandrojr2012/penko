43db817e397c947af1f36c7d7035b618f3ea66ab
leandrojr2012 <106202719+leandrojr2012@users.noreply.github.com>
2023-12-11 21:14:23 -0300

    .

--- src/busca/busca.js
+++ src/busca/busca.js
@@ -1,4 +1,5 @@
 const db = require('../db/dataBase');
+const nodemailer = require('nodemailer')
 
 async function busca_data(){
 
@@ -9,9 +10,9 @@ async function busca_data(){
 
     let data_final = data_ano + '-' + data_mes + '-' + data_dia
     
-    let data_teste = '2023-12-17'
+    let data_teste = '2023-12-21'
 
-    const data = await db.select('entrada_not_cob_data_recebimento')
+    const data = await db.select('entrada_not_cob_data_recebimento', 'entrada_not_cob_identrada')
                          .from('entrada_notificacao_cobranca')
 
         for(let i in data){
@@ -22,10 +23,42 @@ async function busca_data(){
                 let nova_ano = nova_data.getFullYear().toString()
                 let data_banco = nova_ano+'-'+nova_mes+'-'+nova_dia
 
-                if(data_teste == data_banco){
-                    await db('entrada_notificacao_cobranca')
-                    .where({entrada_status_desc_identrada : entrada_status_desc_identrada})
-                    .update({entrada_not_cob_email_enviado:1})
+                console.log('data: ' + data_banco + ' id: ' + data[i].entrada_not_cob_identrada)
+
+                //if(data_teste === data_banco && data[i].entrada_not_cob_email_enviado == 0){
+                    if(data_teste == '2024-01-10'){
+
+                    let config = nodemailer.createTransport({
+                        host: 'smtp.gmail.com',
+                        port: 465,
+                        secure: true,
+                        auth: {
+                            user: 'leandro.daniel.jr@gmail.com',
+                            pass: 'yurf zmik ulao blua',
+                        },
+                        tls: {
+                            rejectUnauthorized: false
+                        }
+                    })
+
+                    config.sendMail({
+                        from:'Leandro_Email <leandro.daniel.jr@gmail.com>',
+                        to:'leandro.daniel.jr@gmail.com',
+                        subject:'teste envio email (id: ' + data[i].entrada_not_cob_identrada + ' )',
+                        html: '<h1>Ola, teste</h1> id:  ' + data[i].entrada_not_cob_identrada  + ' teste email',
+                        text: 'id: ' + data[i].entrada_not_cob_identrada + " teste email *esse;"
+                    })
+                    .then(async()=>{
+                                await db('entrada_notificacao_cobranca')
+                                .where({entrada_not_cob_identrada: entrada_not_cob_identrada})
+                                .update({entrada_not_cob_email_enviado:1})
+                    })
+                    .catch((err)=> console.log(err))
+
+                    /*db('entrada_notificacao_cobranca')
+                    .where({entrada_not_cob_data_recebimento : data_teste})
+                    .where({entrada_not_cob_email_enviado:0})
+                    .update({entrada_not_cob_email_enviado:1})*/
                 }
                 
             }
